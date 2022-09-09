1b183d6415d176e22e2f36b52b00436505476f0f
praiseadegokee <praiseadegokee@gmail.com>
2022-09-09 14:53:20 -0700

    task 0 commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,10 +19,10 @@ int main(void)
 		printf("%d is negative\n", n);
 	} else if (n == 0)
 	{
-		printf("%d is zero/n", n);
+		printf("%d is zero\n", n);
 	} else
 	{
-		printf("%d is positive/n", n);
+		printf("%d is positive\n", n);
 	}
 	return (0);
 }
