91f53db45c2e65f2c6513fba48c2453b1b842ce0
praiseadegokee <praiseadegokee@gmail.com>
2022-09-13 09:49:47 -0700

    task 2 commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ *
+ * Return: always 0;
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char t;
+	char u;
+
+	for (u = 0; u <= 9; u++)
+	{
+		for (t = 'a'; t <= 'z'; t++)
+		{
+			_putchar(t);
+		}
+		_putchar('\n');
+	}
+}
