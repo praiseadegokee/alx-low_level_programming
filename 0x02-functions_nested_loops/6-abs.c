43fc6867f6d257927832ebb9e5c3152649bce1b4
praiseadegokee <praiseadegokee@gmail.com>
2022-09-13 10:41:26 -0700

    functions nested loops task 6 commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - main function
+ *
+ * @a: integer to make absolute
+ * Return: int or int times negative
+ *
+ */
+
+int _abs(int a)
+{
+	if (a > 0)
+		return (a);
+	else
+		return (a * -1);
+}
