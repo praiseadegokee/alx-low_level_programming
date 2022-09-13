e6c4eeb8b52d32d2a466053d00f45f8137366df7
praiseadegokee <praiseadegokee@gmail.com>
2022-09-13 16:03:26 -0700

    functions nested loops, task 8 commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * jack_bauer - main thing
+ *
+ */
+void jack_bauer(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x <= 23; x++)
+	{
+		for (y = 0; y <= 59; y++)
+		{
+			_putchar(x / 10 + '0');
+			_putchar(x % 10 + '0');
+			_putchar(':');
+			_putchar(y / 10 + '0');
+			_putchar(y % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
