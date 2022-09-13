37a47f2e94482e3672d5335d5dc146bf40d71afe
praiseadegokee <praiseadegokee@gmail.com>
2022-09-13 10:19:05 -0700

    task 3 commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,21 +1,18 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
  * _islower - main function
  *
- * @c: character to compare
+ * @t: character to compare
  *
  * Return: 1 if true, 0 if false
  *
  */
 
-int _islower(int c)
+int _islower(int t)
 {
-
-	if (c >= 'a' && c <= 'z')
+	if (t >= 'a' && t <= 'z')
 	{
 		return (1);
 	}
 	return (0);
-
 }
