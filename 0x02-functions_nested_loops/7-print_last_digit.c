efaddb4b7dd50d7fccc8128cebfea545381a4e5e
praiseadegokee <praiseadegokee@gmail.com>
2022-09-13 15:47:55 -0700

    functions nested loops, task 7 commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include "6-abs.c"
+/**
+ * print_last_digit - main function
+ *
+ * @x: integer to get last digit of
+ * Return: last digit of x
+ *
+ */
+
+int print_last_digit(int x)
+{
+	_putchar('0' + _abs(x % 10));
+	return (_abs(x % 10));
+}
