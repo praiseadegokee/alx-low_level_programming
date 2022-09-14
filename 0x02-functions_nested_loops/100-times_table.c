a26ab6dff5f9d26a44056a18b8d20ea2c7fc0522
praiseadegokee <praiseadegokee@gmail.com>
2022-09-13 17:05:16 -0700

    functions nested loops, task 12 commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_times_table - times table function
  * @n: integer to return
@@ -8,6 +9,7 @@ void print_times_table(int n)
 {
 	int i;
 	int j;
+
 	if (n <= 15 && n >= 0)
 	{
 		for (i = 0; i <= n; i++)
