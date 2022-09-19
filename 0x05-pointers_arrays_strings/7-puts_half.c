7cb585f6d286100e4f5d554e49835d50e46af6fb
praiseadegokee <praiseadegokee@gmail.com>
2022-09-19 13:39:05 -0700

    pointers arrays strings, task 7 commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * puts_half - printing other characters of a string
+ * @str: pointer to an int that will change
+ *
+ * Return: void w
+ */
+
+void puts_half(char *str)
+{
+	int a, lastC;
+
+	a = 0;
+	while (str[a] != '\0')
+	{
+		a++;
+	}
+	lastC = (a + 1) / 2;
+
+	for (a = lastC; str[a]; a++)
+	{
+		_putchar (str[a]);
+	}
