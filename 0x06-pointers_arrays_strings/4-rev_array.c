9fb291eb31a77856b8d6a33435e3ceee93f02a84
praiseadegokee <praiseadegokee@gmail.com>
2022-09-22 12:34:27 -0700

    6 pointers arrays strings, task 4, attempt idk error fix

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,18 +1,20 @@
-/*
-reverse_array - reverses an array
- *
- * @a: array to reverse
- * @n: size of array
+#include "main.h"
+
+/**
+ * reverse_array - reverses an array of integers
+ * @a: array to be reversed
+ * @n: number of elements in the array
  */
 void reverse_array(int *a, int n)
 {
-	int i;
-	int t;
+	int i, j, tmp;
+
+	j = n - 1;
 
 	for (i = 0; i < n / 2; i++)
 	{
-		t = a[i];
-		a[i] = a[n - i - 1];
-		a[n - i - 1] = t;
+		tmp = a[i];
+		a[i] = a[j];
+		a[j--] = tmp;
 	}
 }
